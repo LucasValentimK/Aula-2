78a52db40e052c4b7535652daba1c5c02ed9c9ac
LucasValentimK <36766598+LucasValentimK@users.noreply.github.com>
2018-03-02 11:20:22 -0300

    Add files via upload

---
+++ ProjetoSoma/ProjetoSoma/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjetoSoma.Modelo;
+
+namespace ProjetoSoma
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            Controle controle = new Controle();
+            lblResultado.Text = controle.Calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "+");
+        }
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            Controle controle = new Controle();
+            lblResultado.Text = controle.Calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "-");
+
+        }
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            Controle controle = new Controle();
+            lblResultado.Text = controle.Calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "*");
+
+        }
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            Controle controle = new Controle();
+            lblResultado.Text = controle.Calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "/");
+
+        }
+    }
+}
